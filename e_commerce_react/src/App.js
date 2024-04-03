1d8f1f2a05a8a93ea893cfe6597c4b3c7cf796b3
Sasank Das Alladi <alladisasankdas@example.com>
2024-04-02 23:28:53 -0500

    Installed react router dom and defined routes in app js

--- e_commerce_react/src/App.js
+++ e_commerce_react/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import { BrowserRouter as Router, Route,  Routes} from "react-router-dom";
+import Home from './Home' 
+import About from './About'
+import Products from './Products'
+import Contact from './Contact'
+import SingleProduct from './SingleProduct'
+import Cart from './Cart'
+import ErrorPage from './ErrorPage'
 
 function App() {
-  return <></>;
+  return <>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/About" element={<About />}/>
+      <Route path="/products" element={<Products />}/>
+      <Route path="/Contact" element={<Contact />}/>
+      <Route path="/Singleproduct/:id" element={<SingleProduct />}/>
+      <Route path="/Cart" element={<Cart />}/>
+      <Route path="*" element={<ErrorPage />}/>
+    
+    </Routes>
+  </Router>
+  
+  </>;
 }
 
 export default App;
